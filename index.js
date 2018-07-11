796b494dfe03a09ec11816b5aa68ba106eefed03
David Eliason <david@davethemaker.com>
2018-07-11 15:55:55 -0700

    add img to static_files subfolder

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ const AWS = require('aws-sdk');
 const fs = require('fs');
 const path = require('path');
 const misc = require('./s3upload');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
 
 require('dotenv').config()
+// misc.s3upload();
+app.use(express.static(__dirname + "../static_files"));
+// routes
+app.get('/', (req,res) => {
+  res.end('hello world');
+});
 
-misc.s3upload();
+app.listen(port);
+console.log(`server listening at ${port}`);
