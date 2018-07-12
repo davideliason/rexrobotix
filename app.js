7e8f4c0e83bec0c77f6b521c01947d641955ef0b
David Eliason <david@davethemaker.com>
2018-07-12 05:25:01 -0700

    refactored so that the initially served html doc has a multipart/form-data enctype form that allows the user to upload a photo using multer to a public/subfolder directory. Currently, that successfully uploaded photo is being linked to in a hardcoded manner

--- app.js
+++ app.js
@@ -1,28 +1,69 @@
-var express = require('express');
-var session = require('express-session');
-var path = require('path');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var fs = require('fs');
-var multer = require('multer');
-var upload = multer({
+// RUN PACKAGES
+
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const multer = require('multer');
+const upload = multer({
 	dest: "uploads/"
 });
 
-var app = express()
+// SETUP APP
+const app = express();
+const port = process.env.PORT || 8080;
 
 // middleware
-   .use(logger('dev'))
-   .use(bodyParser.json())
-   .use(bodyParser.urlencoded({ extended: false }))
+app.use(logger('dev'))
+app.use(bodyParser.json()) // JSON
+app.use(bodyParser.urlencoded({ extended: false })) // body requests
 // app.use(cookieParser())
-   // .use(express.static(path.join(__dirname,'public')))
-   .use(session({
-		secret: "asdfasd",
-		resave: false,
-		saveUninitiated: true,
-		cookie: { maxAge: 180000}
-	}));
+// .use(express.static(path.join(__dirname,'public')))
+app.use(session({
+	secret: "asdfasd",
+	resave: false,
+	saveUninitiated: true,
+	cookie: { maxAge: 180000}
+}))
+app.use('/', express.static(__dirname + '/public')); 
+
+
+const multerConfig = {
+    
+storage: multer.diskStorage({
+ //Setup where the user's file will go
+ destination: function(req, file, next){
+   next(null, './public/photo-storage');
+   },   
+    
+    //Then give the file a unique name
+    filename: function(req, file, next){
+        console.log(file);
+        const ext = file.mimetype.split('/')[1];
+        next(null, file.fieldname + '-' + Date.now() + '.'+ext);
+      }
+    }),   
+    
+    //A means of ensuring only images are uploaded. 
+    fileFilter: function(req, file, next){
+          if(!file){
+            next();
+          }
+        const image = file.mimetype.startsWith('image/');
+        if(image){
+          console.log('photo uploaded');
+          next(null, true);
+        }else{
+          console.log("file not supported");
+          
+          //TODO:  A better message response to user on failure.
+          return next();
+        }
+    }
+  };
+
 
    // Sessions
    app.get('/',function(req,res) {
@@ -33,7 +74,7 @@ var app = express()
    	fs.writeFile('session.txt',x.toString(), (err) => {
    		if(err) console.log(err);
    	});
-   	res.send(`you logged on ${x}`);
+   	res.render('index.html');
    });
 
    app.get('/loggedin', function(req,res,next) {
@@ -51,7 +92,16 @@ var app = express()
    	res.end("You uploaded" + req.file.originalname);
    });
 
-   app.listen(8080);
+    app.post('/upload', multer(multerConfig).single('photo'),function(req, res){
+      //Here is where I could add functions to then get the url of the new photo
+      //And relocate that to a cloud storage solution with a callback containing its new url
+      //then ideally loading that into your database solution.   Use case - user uploading an avatar...
+      res.send('upload complete');
+  }
+
+);
+
+   app.listen(port);
 
 
 
