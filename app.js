d85cfba0693ab7e5a1462bc1ae06fd89fae2ff92
David Eliason <david@davethemaker.com>
2018-07-11 19:17:58 -0700

    add sample session cookie for testing and learning

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
 var app = express();
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(session({ secret: 'this-is-a-secret-token', cookie: { maxAge: 60000 }}));
+// middleware
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(cookieParser())
+app.use(express.static(path.join(__dirname,'public')))
 
-app.get('/', (req,res) => {
-	res.sendFile('/index.html');
+
+
+// routes
+// app.get('/', (req,res) => {
+// 	res.sendFile('/index.html');
+// });
+app.get('/', function(req, res, next) {
+  var sessData = req.session;
+  sessData.someAttribute = "foo";
+  res.send('Returning with some text');
+});
+
+app.get('/bar', function(req, res, next) {
+  var someAttribute = req.session.someAttribute;
+  res.send(`This will print the attribute I set earlier: ${someAttribute}`);
 });
 
 app.listen(8080);
